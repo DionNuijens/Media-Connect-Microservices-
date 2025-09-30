570f4c961c594397c49e7ba4b8a62589f1968260
DionNuijens <d.nuijens@student.fontys.nl>
2025-09-30 21:23:56 +0200

    test all the yaml files

--- Media-Service/src/test/java/org/example/mediaconnect/MediaConnectApplicationTests.java
+++ Media-Service/src/test/java/org/example/mediaconnect/MediaConnectApplicationTests.java
@@ -3,7 +3,16 @@ package org.example.mediaconnect;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {
+                "spring.datasource.url=jdbc:h2:mem:testdb",
+                "spring.datasource.driverClassName=org.h2.Driver",
+                "spring.datasource.username=sa",
+                "spring.datasource.password=",
+                "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+        }
+)
 class MediaConnectApplicationTests {
 
 	@Test
--- Social-Service/src/test/java/org/example/socialservice/SocialServiceApplicationTests.java
+++ Social-Service/src/test/java/org/example/socialservice/SocialServiceApplicationTests.java
@@ -3,7 +3,16 @@ package org.example.socialservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.NONE,
+        properties = {
+                "spring.datasource.url=jdbc:h2:mem:testdb",
+                "spring.datasource.driverClassName=org.h2.Driver",
+                "spring.datasource.username=sa",
+                "spring.datasource.password=",
+                "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+        }
+)
 class SocialServiceApplicationTests {
 
     @Test
