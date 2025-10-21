551554a664ca833b7e6c1990ce6ca4a369ab10ee
DionNuijens <d.nuijens@student.fontys.nl>
2025-10-21 20:30:03 +0200

    Add getter and setter to user

--- Auth-Service/src/main/java/org/example/authservice/model/User.java
+++ Auth-Service/src/main/java/org/example/authservice/model/User.java
@@ -1,16 +1,15 @@
 package org.example.authservice.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
+@Setter
 
 public class User {
     @Id
